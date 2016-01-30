78f196a1229822deb45d45b9f79887f82336428a
Alex W. Lulin <lulinalex@gmail.com>
2016-01-30 14:41:00 +0300

    - обновление.

---
+++ shared/models/NewSchool/Scripts/cal.cmd
@@ -0,0 +1,5 @@
+pushd W:\shared\models\NewSchool\Scripts
+call clear.cmd
+start "" W:\common\env\QuickLaunch\callMSS.exe "%~1"
+rem call clearempty.cmd
+popd
--- shared/models/NewSchool/Scripts/cshd.cmd
+++ shared/models/NewSchool/Scripts/cshd.cmd
@@ -1,5 +1,7 @@
 pushd W:\shared\models\NewSchool\Scripts
 call clear.cmd
 W:\common\env\QuickLaunch\callMSS.exe -list:W:\shared\models\NewSchool\Scripts\SHD_K2_Everest.script.list
+rem start "" W:\common\env\QuickLaunch\callMSS.exe -list:W:\shared\models\NewSchool\Scripts\SHD_K2_Everest1.script.list
+rem start "" W:\common\env\QuickLaunch\callMSS.exe -list:W:\shared\models\NewSchool\Scripts\SHD_K2_Everest2.script.list
 rem call clearempty.cmd
 popd
