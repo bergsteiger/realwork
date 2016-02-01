53411ce801f565e0141789c45dda0a5d9ed3f131
Alex W. Lulin <lulinalex@gmail.com>
2016-02-02 01:27:53 +0300

    - перегенерация.

---
+++ common/GenScripts/ME_56AF378901B9.pas
@@ -0,0 +1,105 @@
+unit arBelaPubList;
+
+// ћодуль: "w:\common\components\rtl\Garant\ar\arBelaPubList.pas"
+// —тереотип: "SimpleClass"
+
+{$Include l3Define.inc}
+
+interface
+
+{$If NOT Defined(Nemesis)}
+uses
+ l3IntfUses
+ , l3ProtoDataContainer
+ , l3Memory
+ , l3Types
+ , l3Interfaces
+ , l3Core
+ , l3Except
+ , Classes
+ , l3Date
+;
+
+type
+ TarBelaPubInfo = record
+  rPubSource: Il3CString;
+   {* IST Ц источник публикации правового акта (код значение) (м); }
+  rDate: TStDate;
+   {* IDAT Ц дата издани€ источника публикации (M); }
+  rYear: Word;
+   {* YEAR Ц год публикации (M); }
+  rNum: Integer;
+   {* NUMB Ц номер источника публикации (M); }
+  rArticle: Integer;
+   {* IN Ц номер статьи (M). ѕри отсутствии информации о номере статьи за идентификатором пол€ через пробел ставитс€ символ Ђ-ї (минус); }
+ end;//TarBelaPubInfo
+
+ ParBelaPubInfo = ^TarBelaPubInfo;
+
+ _ItemType_ = TarBelaPubInfo;
+ _l3RecordListPrim_Parent_ = Tl3ProtoDataContainer;
+ {$Define l3Items_IsProto}
+ {$Include l3RecordListPrim.imp.pas}
+ TarBelaPubList = class(_l3RecordListPrim_)
+ end;//TarBelaPubList
+{$IfEnd} // NOT Defined(Nemesis)
+
+implementation
+
+{$If NOT Defined(Nemesis)}
+uses
+ l3ImplUses
+ , l3String
+ , l3Base
+ , l3MinMax
+ , RTLConsts
+ , SysUtils
+;
+
+{$If Defined(l3Items_NeedsAssignItem) AND NOT Defined(l3Items_NoSort)}
+procedure AssignItem(const aTo: _ItemType_;
+ const aFrom: _ItemType_); forward;
+{$IfEnd} // Defined(l3Items_NeedsAssignItem) AND NOT Defined(l3Items_NoSort)
+function CompareExistingItems(const CI: CompareItemsRec): Integer; forward;
+
+{$If NOT Defined(l3Items_IsAtomic)}
+function IsSameItems(const A: _ItemType_;
+ const B: _ItemType_): Boolean;
+ {* —равнивает элементы списка }
+//#UC START# *47B07CF403D0_56AF378901B9_var*
+//#UC END# *47B07CF403D0_56AF378901B9_var*
+begin
+//#UC START# *47B07CF403D0_56AF378901B9_impl*
+ !!! Needs to be implemented !!!
+//#UC END# *47B07CF403D0_56AF378901B9_impl*
+end;//IsSameItems
+{$IfEnd} // NOT Defined(l3Items_IsAtomic)
+
+{$If Defined(l3Items_NeedsAssignItem) AND NOT Defined(l3Items_NoSort)}
+procedure AssignItem(const aTo: _ItemType_;
+ const aFrom: _ItemType_);
+//#UC START# *47B2C42A0163_56AF378901B9_var*
+//#UC END# *47B2C42A0163_56AF378901B9_var*
+begin
+//#UC START# *47B2C42A0163_56AF378901B9_impl*
+ !!! Needs to be implemented !!!
+//#UC END# *47B2C42A0163_56AF378901B9_impl*
+end;//AssignItem
+{$IfEnd} // Defined(l3Items_NeedsAssignItem) AND NOT Defined(l3Items_NoSort)
+
+function CompareExistingItems(const CI: CompareItemsRec): Integer;
+ {* —равнивает два существующих элемента. }
+//#UC START# *47B99D4503A2_56AF378901B9_var*
+//#UC END# *47B99D4503A2_56AF378901B9_var*
+begin
+//#UC START# *47B99D4503A2_56AF378901B9_impl*
+ !!! Needs to be implemented !!!
+//#UC END# *47B99D4503A2_56AF378901B9_impl*
+end;//CompareExistingItems
+
+type _Instance_R_ = TarBelaPubList;
+
+{$Include l3RecordListPrim.imp.pas}
+{$IfEnd} // NOT Defined(Nemesis)
+
+end.
