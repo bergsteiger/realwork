bdb0639a193ecfea4b07440498195db05ef51d62
Alex W. Lulin <lulinalex@gmail.com>
2016-02-09 22:30:19 +0300

    - обновление.

--- shared/models/NewSchool/Scripts/cc.cmd
+++ shared/models/NewSchool/Scripts/cc.cmd
@@ -1,13 +1,18 @@
-call c.cmd 4ABCC25A0322.pas.ms.script.runner
+call c.cmd 4F4763EC02CB.pas.ms.script.runner
+
+rem call c.cmd 4DAF19F600C5.pas.ms.script.runner
+rem tfwScriptingInterfaces
+
+rem call c.cmd 4ABCC25A0322.pas.ms.script.runner
 rem Shared Delphi Low Level
 
 rem call c.cmd 4790D09A0378.pas.ms.script.runner
 rem L3
 
-call c.cmd 4677E16A022A.pas.ms.script.runner
+rem call c.cmd 4677E16A022A.pas.ms.script.runner
 rem K2
 
-call c.cmd 4797632D0273.pas.ms.script.runner
+rem call c.cmd 4797632D0273.pas.ms.script.runner
 rem Everest
 
 rem call c.cmd 48342A280198.pas.ms.script.runner
