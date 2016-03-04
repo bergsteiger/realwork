766afb9c2a721a893461f682228027848aac2224
Alex W. Lulin <lulinalex@gmail.com>
2016-03-04 04:46:41 +0300

    - перегенерация.

--- garant6x/implementation/Garant/GbaNemesis/F1_Words/f1WordsPack.pas
+++ garant6x/implementation/Garant/GbaNemesis/F1_Words/f1WordsPack.pas
@@ -1,58 +1,49 @@
 unit f1WordsPack;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// Библиотека "F1 Words"
-// Автор: Люлин А.В.
-// Модуль: "w:/garant6x/implementation/Garant/GbaNemesis/F1_Words/f1WordsPack.pas"
-// Начат: 27.04.2011 14:53
-// Родные Delphi интерфейсы (.pas)
-// Generated from UML model, root element: <<ScriptKeywordsPack::Class>> F1 Поддержка тестов::F1 Words::Words::f1WordsPack
-//
-//
-// Все права принадлежат ООО НПП "Гарант-Сервис".
-//
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// ! Полностью генерируется с модели. Править руками - нельзя. !
+// Модуль: "w:\garant6x\implementation\Garant\GbaNemesis\F1_Words\f1WordsPack.pas"
+// Стереотип: "ScriptKeywordsPack"
 
 {$Include w:\garant6x\implementation\Garant\GbaNemesis\nsDefine.inc}
 
 interface
 
-{$If not defined(NoScripts)}
-{$IfEnd} //not NoScripts
+{$If NOT Defined(NoScripts)}
+uses
+ l3IntfUses
+;
+{$IfEnd} // NOT Defined(NoScripts)
 
 implementation
 
-{$If not defined(NoScripts)}
+{$If NOT Defined(NoScripts)}
 uses
-  kwDocumentsCacheClear,
-  kwTimeMachineSetDate,
-  kwPopEditorHasComment,
-  kwExportHelp,
-  kwFiltersList,
-  kwF1BaseDate,
-  kwAttributeSearch,
-  kwOk,
-  kwMainMenu,
-  kwF1ComplectName,
-  kwF1ComplectOwner,
-  kwRestoreSettings,
-  kwClearDocumentContextSearchHistory,
-  kwClearPharmContextSearchHistory,
-  kwIsPharmPermanentFilterExists,
-  kwIsLegalPermanentFilterExists,
-  kwIsMyDocumentsEmpty,
-  kwConfigurationsGetCount,
-  kwConfigurationsGetConfiguration,
-  kwConfigurationGetName,
-  kwConfigurationGetType,
-  kwConfigurationsGetActiveConfiguration,
-  kwConfigurationActivate,
-  kwConfigurationGetID,
-  tfwScriptingTypes
-  ;
+ l3ImplUses
+ , kwDocumentsCacheClear
+ , kwTimeMachineSetDate
+ , kwPopEditorHasComment
+ , kwExportHelp
+ , kwFiltersList
+ , kwF1BaseDate
+ , kwAttributeSearch
+ , kwOk
+ , kwMainMenu
+ , kwF1ComplectName
+ , kwF1ComplectOwner
+ , kwRestoreSettings
+ , kwClearDocumentContextSearchHistory
+ , kwClearPharmContextSearchHistory
+ , kwIsPharmPermanentFilterExists
+ , kwIsLegalPermanentFilterExists
+ , kwIsMyDocumentsEmpty
+ , kwConfigurationsGetCount
+ , kwConfigurationsGetConfiguration
+ , kwConfigurationGetName
+ , kwConfigurationGetType
+ , kwConfigurationsGetActiveConfiguration
+ , kwConfigurationActivate
+ , kwConfigurationGetID
+ , tfwScriptingTypes
+;
+{$IfEnd} // NOT Defined(NoScripts)
 
-{$IfEnd} //not NoScripts
 end.
