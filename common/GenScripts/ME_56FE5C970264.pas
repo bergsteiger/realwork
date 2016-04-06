6c5e33f375dd70b16a5dda9859d8fb92f4cd9eea
Alex W. Lulin <lulinalex@gmail.com>
2016-04-07 01:28:16 +0300

    - перегенерация.

--- common/GenScripts/ME_56FE5C970264.pas
+++ common/GenScripts/ME_56FE5C970264.pas
@@ -20,11 +20,16 @@ type
  TcsContainerTask = class(TddProcessTask)
   protected
    function pm_GetTasksList: TasksListHelper;
+   function pm_GetParallelExecutionMode: Boolean;
+   procedure pm_SetParallelExecutionMode(aValue: Boolean);
   public
    class function GetTaggedDataType: Tk2Type; override;
   public
    property TasksList: TasksListHelper
     read pm_GetTasksList;
+   property ParallelExecutionMode: Boolean
+    read pm_GetParallelExecutionMode
+    write pm_SetParallelExecutionMode;
  end;//TcsContainerTask
 {$IfEnd} // NOT Defined(Nemesis)
 
@@ -44,6 +49,24 @@ begin
  !!! Needs to be implemented !!!
 //#UC END# *807379469BD5_56FE5C970264get_impl*
 end;//TcsContainerTask.pm_GetTasksList
+
+function TcsContainerTask.pm_GetParallelExecutionMode: Boolean;
+//#UC START# *35FA81042B4C_56FE5C970264get_var*
+//#UC END# *35FA81042B4C_56FE5C970264get_var*
+begin
+//#UC START# *35FA81042B4C_56FE5C970264get_impl*
+ !!! Needs to be implemented !!!
+//#UC END# *35FA81042B4C_56FE5C970264get_impl*
+end;//TcsContainerTask.pm_GetParallelExecutionMode
+
+procedure TcsContainerTask.pm_SetParallelExecutionMode(aValue: Boolean);
+//#UC START# *35FA81042B4C_56FE5C970264set_var*
+//#UC END# *35FA81042B4C_56FE5C970264set_var*
+begin
+//#UC START# *35FA81042B4C_56FE5C970264set_impl*
+ !!! Needs to be implemented !!!
+//#UC END# *35FA81042B4C_56FE5C970264set_impl*
+end;//TcsContainerTask.pm_SetParallelExecutionMode
 
 class function TcsContainerTask.GetTaggedDataType: Tk2Type;
 //#UC START# *53AC03EE01FD_56FE5C970264_var*
