537f4db50eeadda0327d75562824efb64898df48
Alex W. Lulin <lulinalex@gmail.com>
2016-04-07 23:11:43 +0300

    - перегенерация.

--- shared/models/NewSchool/Scripts/cag.cmd
+++ shared/models/NewSchool/Scripts/cag.cmd
@@ -1,4 +1,5 @@
 pushd W:\shared\models\NewSchool\Scripts
+call clearbackup.cmd
 call clear.cmd
 W:\common\env\QuickLaunch\callMSS.exe -list:W:\shared\models\NewSchool\Scripts\Generated.script.list
 rem call clearempty.cmd
--- shared/models/NewSchool/Scripts/cshd.cmd
+++ shared/models/NewSchool/Scripts/cshd.cmd
@@ -1,4 +1,5 @@
 pushd W:\shared\models\NewSchool\Scripts
+call clearbackup.cmd
 call clear.cmd
 W:\common\env\QuickLaunch\callMSS.exe -list:W:\shared\models\NewSchool\Scripts\SHD_K2_Everest.script.list
 rem start "" W:\common\env\QuickLaunch\callMSS.exe -list:W:\shared\models\NewSchool\Scripts\SHD_K2_Everest1.script.list
