e1f0a7fdb718566908074ecc208bc65622702e82
Alex W. Lulin <lulinalex@gmail.com>
2016-04-19 00:21:02 +0300

    - перегенерация.

--- shared/models/NewSchool/Scripts/gen.cmd
+++ shared/models/NewSchool/Scripts/gen.cmd
@@ -1,8 +1,8 @@
-del found.script.list
-findstr /M /C:"CONST Name '%~1'" *.pas.ms.script>> found.script.list
-findstr /M /C:"CONST extprop:pas:TypeName '%~1'" *.pas.ms.script>> found.script.list
-findstr /M /C:"CONST UID '%~1'" *.pas.ms.script>> found.script.list
-rem grep -l+ "'%~1'" *.pas.ms.script > found.script.list
-del torun.script.list
-for /f %%i in (found.script.list) do echo %%i.runner>> torun.script.list
-call cal.cmd -list:torun.script.list
+del %~1.script.list
+findstr /M /C:"CONST Name '%~1'" *.pas.ms.script>> %~1.script.list
+findstr /M /C:"CONST extprop:pas:TypeName '%~1'" *.pas.ms.script>> %~1.script.list
+findstr /M /C:"CONST UID '%~1'" *.pas.ms.script>> %~1.script.list
+rem grep -l+ "'%~1'" *.pas.ms.script > %~1.script.list
+del run%~1.script.list
+for /f %%i in (%~1.script.list) do echo %%i.runner>> run%~1.script.list
+call cal.cmd -list:run%~1.script.list
--- shared/models/NewSchool/Scripts/gens.cmd
+++ shared/models/NewSchool/Scripts/gens.cmd
@@ -1,6 +1,6 @@
-del found.script.list
-findstr /M /C:"Stereotype st_%~1" *.pas.ms.script>> found.script.list
-rem grep -l+ "Stereotype st_%~1" *.pas.ms.script>> found.script.list
-del torun.script.list
-for /f %%i in (found.script.list) do echo %%i.runner>> torun.script.list
-call cal.cmd -list:torun.script.list
+del %~1.script.list
+findstr /M /C:"Stereotype st_%~1" *.pas.ms.script>> %~1.script.list
+rem grep -l+ "Stereotype st_%~1" *.pas.ms.script>> %~1.script.list
+del run%~1.script.list
+for /f %%i in (%~1.script.list) do echo %%i.runner>> run%~1.script.list
+call cal.cmd -list:run%~1.script.list
