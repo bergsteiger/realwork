7445b9898915e4016a3fcfb9b9b485256751a88f
Alex W. Lulin <lulinalex@gmail.com>
2016-05-31 00:43:07 +0300

    - обновление.

--- shared/models/NewSchool/Scripts/genTargets.cmd
+++ shared/models/NewSchool/Scripts/genTargets.cmd
@@ -1,12 +1,4 @@
 pushd W:\shared\models\NewSchool\Scripts
-del targets.script.list
 call makeInject.cmd
-findstr /M /C:"Stereotype st_ExeTarget" *.pas.ms.script>> targets.script.list
-findstr /M /C:"Stereotype st_AdapterTarget" *.pas.ms.script>> targets.script.list
-findstr /M /C:"Stereotype st_TestTarget" *.pas.ms.script>> targets.script.list
-findstr /M /C:"Stereotype st_VCMTestTarget" *.pas.ms.script>> targets.script.list
-findstr /M /C:"Stereotype st_VCMGUI" *.pas.ms.script>> targets.script.list
-del targetstorun.script.list
-for /f %%i in (targets.script.list) do echo %%i.runner>> targetstorun.script.list
-call cal.cmd -list:targetstorun.script.list
+call genTargetsPrim.cmd
 popd
