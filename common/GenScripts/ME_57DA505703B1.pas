00c3c7162fddf019846431dec662cdf557fffa74
Alex W. Lulin <lulinalex@gmail.com>
2016-09-16 00:43:51 +0300

    - перегенерация.

---
+++ common/GenScripts/ME_57DA505703B1.pas
@@ -0,0 +1,29 @@
+unit csMultiModifyDocs_Const;
+
+// Модуль: "w:\common\components\rtl\Garant\EVD\csMultiModifyDocs_Const.pas"
+// Стереотип: "Tag"
+// Элемент модели: "csMultiModifyDocs" MUID: (57DA505703B1)
+// Имя типа: "csMultiModifyDocsTag"
+
+{$Include w:\common\components\rtl\Garant\EVD\evdDefine.inc}
+
+interface
+
+uses
+ l3IntfUses
+ , csMessageWithReply_Const
+ , k2RawData_Const
+ , k2Long_Const
+ , Address_Const
+ , l3MarshalledTypes
+;
+
+ // csMultiModifyDocsTag
+
+implementation
+
+uses
+ l3ImplUses
+;
+
+end.
---
+++ common/GenScripts/ME_57DA507703CD.pas
@@ -0,0 +1,28 @@
+unit csMultiModifyDocsReply_Const;
+
+// Модуль: "w:\common\components\rtl\Garant\EVD\csMultiModifyDocsReply_Const.pas"
+// Стереотип: "Tag"
+// Элемент модели: "csMultiModifyDocsReply" MUID: (57DA507703CD)
+// Имя типа: "csMultiModifyDocsReplyTag"
+
+{$Include w:\common\components\rtl\Garant\EVD\evdDefine.inc}
+
+interface
+
+uses
+ l3IntfUses
+ , csReply_Const
+ , Address_Const
+ , k2Bool_Const
+ , k2String_Const
+;
+
+ // csMultiModifyDocsReplyTag
+
+implementation
+
+uses
+ l3ImplUses
+;
+
+end.
