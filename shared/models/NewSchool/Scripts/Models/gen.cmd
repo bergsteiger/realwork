4361364a59c8c9a1b85737ec204f1af6c4d6f635
Alex W. Lulin <lulinalex@gmail.com>
2016-10-14 00:54:54 +0300

    - обновление.

--- shared/models/NewSchool/Scripts/Models/gen.cmd
+++ shared/models/NewSchool/Scripts/Models/gen.cmd
@@ -1,8 +1,10 @@
 del C:\Temp\%~1.script.list
 findstr /M /C:"CONST Name '%~1'" *.ms.model.script>> C:\Temp\%~1.script.list
+findstr /M /C:"CONST OriginalName '%~1'" *.ms.model.script>> C:\Temp\%~1.script.list
 findstr /M /C:"CONST extprop:pas:TypeName '%~1'" *.ms.model.script>> C:\Temp\%~1.script.list
 findstr /M /C:"CONST UID '%~1'" *.ms.model.script>> C:\Temp\%~1.script.list
 findstr /M /C:"MEPROP Name '%~1'" *.ms.model.script>> C:\Temp\%~1.script.list
+findstr /M /C:"MEPROP OriginalName '%~1'" *.ms.model.script>> C:\Temp\%~1.script.list
 findstr /M /C:"MEPROP extprop:pas:TypeName '%~1'" *.ms.model.script>> C:\Temp\%~1.script.list
 findstr /M /C:"MEPROP UID '%~1'" *.ms.model.script>> C:\Temp\%~1.script.list
 del C:\Temp\run%~1.script.list
